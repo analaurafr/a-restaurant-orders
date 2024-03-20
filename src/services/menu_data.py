b9fa77f51e6ff7ba0bd698c5106c627727d524b5
analaurafr <laurafernandes@outlook.com>
2024-03-20 17:57:00 -0300

    req 3

--- src/services/menu_data.py
+++ src/services/menu_data.py
@@ -1,4 +1,24 @@
-# Req 3
+import csv
+from models.dish import Dish
+from models.ingredient import Ingredient
+
+
 class MenuData:
-    def __init__(self, source_path: str) -> None:
-        pass
+    def __init__(self, menu_file_path: str) -> None:
+        dish_collection = {}
+        with open(menu_file_path, encoding="UTF-8") as menu_file:
+            menu_data = csv.DictReader(menu_file)
+            for row in menu_data:
+                dish_title = row["dish"]
+                dish_price = float(row["price"])
+                dish_ingredient = row["ingredient"]
+                ingredient_quantity = int(row["recipe_amount"])
+
+                if dish_title not in dish_collection:
+                    dish_collection[dish_title] = Dish(dish_title, dish_price)
+
+                dish_collection[dish_title].add_ingredient_dependency(
+                    Ingredient(dish_ingredient), ingredient_quantity
+                )
+
+        self.dishes = set(dish_collection.values())
