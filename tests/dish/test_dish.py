d4459ca5f50bcfcda643c866ed820a06d7e5fc10
analaurafr <laurafernandes@outlook.com>
2024-03-20 16:59:30 -0300

    req testes

--- tests/dish/test_dish.py
+++ tests/dish/test_dish.py
@@ -1,6 +1,40 @@
+import pytest
 from src.models.dish import Dish  # noqa: F401, E261, E501
+from src.models.ingredient import Ingredient
 
 
 # Req 2
 def test_dish():
-    pass
+
+    dish = Dish("Lasanha", 25.0)
+    assert dish.name == "Lasanha"
+
+    dish1 = Dish("Pizza", 30.0)
+    dish2 = Dish("Pizza", 30.0)
+    assert hash(dish1) == hash(dish2)
+
+    dish3 = Dish("Hambúrguer", 20.0)
+    assert hash(dish1) != hash(dish3)
+
+    assert dish1 == dish2
+
+    assert dish1 != dish3
+
+    assert repr(dish1) == "Dish('Pizza', R$30.00)"
+
+    with pytest.raises(TypeError):
+        Dish("Lasanha", "25.0")
+
+    with pytest.raises(ValueError):
+        Dish("Lasanha", -25.0)
+
+    dish.add_ingredient_dependency(Ingredient("Carne"), 200)
+    assert dish.recipe.get(Ingredient("Carne")) == 200
+
+    assert dish.get_restrictions() == set()
+
+    assert dish.get_ingredients() == {Ingredient("Carne")}
+
+
+if __name__ == "__main__":
+    pytest.main()
--- tests/ingredient/test_ingredient.py
+++ tests/ingredient/test_ingredient.py
@@ -4,69 +4,17 @@ from src.models.ingredient import (
 )  # noqa: F401, E261, E501
 
 
-# Req 1
 def test_ingredient():
-    ingredient = Ingredient("bacon")
-    assert ingredient.name == "bacon"
-
-
-def test_ingredient_restrictions():
-    ingredient = Ingredient("bacon")
-    assert ingredient.restrictions == {
+    ingredient_1 = Ingredient("frango")
+    ingredient_2 = Ingredient("ovo")
+    ingredient_3 = Ingredient("frango")
+    assert ingredient_1 == ingredient_3
+    assert ingredient_1 != ingredient_2
+    assert hash(ingredient_1) != hash(ingredient_2)
+    assert hash(ingredient_1) == hash(ingredient_3)
+    assert ingredient_1.name == "frango"
+    assert repr(ingredient_1) == "Ingredient('frango')"
+    assert ingredient_1.restrictions == {
         Restriction.ANIMAL_MEAT,
         Restriction.ANIMAL_DERIVED,
     }
-
-
-def test_ingredient_repr():
-    ingredient = Ingredient("bacon")
-    assert repr(ingredient) == "Ingredient('bacon')"
-
-
-def test_ingredient_eq_same_object():
-    ingredient1 = Ingredient("bacon")
-    ingredient2 = ingredient1
-    assert ingredient1 == ingredient2
-
-
-def test_ingredient_eq_different_objects_same_name():
-    ingredient1 = Ingredient("bacon")
-    ingredient2 = Ingredient("bacon")
-    assert ingredient1 == ingredient2
-
-
-def test_ingredient_eq_different_objects_different_names():
-    ingredient1 = Ingredient("bacon")
-    ingredient2 = Ingredient("salmão")
-    assert not ingredient1 == ingredient2
-
-
-def test_ingredient_hash():
-    ingredient1 = Ingredient("bacon")
-    ingredient2 = Ingredient("bacon")
-    assert hash(ingredient1) == hash(ingredient2)
-
-
-def test_ingredient_name():
-    ingredient = Ingredient("bacon")
-    assert ingredient.name == "bacon"
-
-
-def test_ingredient_restrictions_correct_values():
-    ingredient = Ingredient("bacon")
-    assert ingredient.restrictions == {
-        Restriction.ANIMAL_MEAT,
-        Restriction.ANIMAL_DERIVED,
-    }
-
-
-def test_ingredient_failure_different_hash_same_ingredient():
-    ingredient1 = Ingredient("bacon")
-    ingredient2 = Ingredient("bacon")
-    assert hash(ingredient1) != hash(ingredient2)
-
-
-def test_ingredient_failure_same_hash_different_ingredients():
-    ingredient1 = Ingredient("bacon")
-    ingredient2 = Ingredient("salmão")
-    assert hash(ingredient1) != hash(ingredient2)
